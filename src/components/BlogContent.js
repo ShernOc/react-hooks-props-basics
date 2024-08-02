b517ca0077217bf4537fc0f1bf7e7342202c00ed
Sherlyne <sherlynea1994@hotmail.com>
2024-08-02 17:40:47 +0300

    add some comments

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+  //the {props.articleText} tells react to place the value of prop.articleText represent withing the <div>
+/*
+function ParentComponent() {
+  passing prop to ChildComponent
+  return <ChildComponent text="Hello!" number={2} />;
+}
+
+function ChildComponent(props) {
+   using the values of the text and number props
+  return (
+    <div>
+      {props.text} {props.number}
+    </div>
+  );
+}*/
+
+
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  return <div id="blog-content">
+    {props.articleText}</div>; 
 }
 
 export default BlogContent;
